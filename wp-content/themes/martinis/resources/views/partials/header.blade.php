f331291cc220af67901bde8960c5a2d389fd0150
Rosita Calderon <rositac@beekeepergroup.com>
2018-03-22 07:53:10 -0400

    Add Bootstrap menu

--- wp-content/themes/martinis/resources/views/partials/header.blade.php
+++ wp-content/themes/martinis/resources/views/partials/header.blade.php
@@ -1,10 +1,14 @@
-<header class="banner">
-  <div class="container">
-    <a class="brand" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
-    <nav class="nav-primary">
+<header>
+  <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+    <a class="navbar-brand" href="#">Carousel</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarCollapse">
+      <!-- composer require "mwdelaney/sage-bootstrap4-navwalker" -->
       @if (has_nav_menu('primary_navigation'))
-        {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
+        {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'navbar-nav nav pull-right', 'walker' => new \App\wp_bootstrap4_navwalker()]) !!}
       @endif
-    </nav>
-  </div>
+    </div>
+  </nav>
 </header>
